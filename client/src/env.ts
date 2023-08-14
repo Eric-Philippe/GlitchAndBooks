281499eb3c77a896190e5cb423c009e6cb118e89
Eric-Philippe <ericphlpp@gmail.com>
2023-08-14 18:26:52 +0200

    root

--- client/src/env.ts
+++ client/src/env.ts
@@ -1,9 +1,9 @@
-const { APP_API_URL } = process.env;
+const { REACT_API_URL } = process.env;
 
-if (!APP_API_URL) {
+if (!REACT_API_URL) {
   throw new Error("APP_API_URL is not defined");
 }
 
-console.log("APP_API_URL", APP_API_URL);
+console.log("APP_API_URL", REACT_API_URL);
 
-export { APP_API_URL };
+export { REACT_API_URL as APP_API_URL };
